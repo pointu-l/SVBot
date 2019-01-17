cd0bd5a04438c5e1b08e4e03b8cff4c34b871791
Lucas Pointurier <pointu_l@etna-alternance.net>
2019-01-17 23:06:12 +0100

    Fix negative moment diff #2

--- src/SVBot.ts
+++ src/SVBot.ts
@@ -151,7 +151,7 @@ class SVBot
                 .hours(this.morningHour.h)
                 .minutes(this.morningHour.m);
             const next: Date = mNext.toDate();
-            now.setMinutes(0);
+            // now.setMinutes(0);
             let ms: number = moment(now,"DD/MM/YYYY HH:mm:ss").diff(next, "ms", true);
             if (ms < 0)
             {
