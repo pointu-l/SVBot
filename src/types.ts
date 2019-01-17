14170cdb3db157a70ffc7af02f20062b05709bb7
Lucas Pointurier <pointu_l@etna-alternance.net>
2019-01-17 09:25:39 +0100

    Forgotten file

---
+++ src/types.ts
@@ -0,0 +1,4 @@
+declare module "hours-objects" {
+    export interface HourObject { h: number, m: number }
+    export interface InitObject { morningHour: string, lunchHour: string, afternoonHour: string, endOfDayHour: string }
+}
